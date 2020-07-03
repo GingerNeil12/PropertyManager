6869ecafbc839d05222eb4c31ee0640a28dc7f17
Neil Earlam <neil.earlam2019@outlook.com>
2020-07-03 14:15:05 +0100

    Added CreateLandlordNotification

---
+++ PropertyManager/PropertyManager.ViewModels/Application/Landlords/Commands/CreateLandlordNotifcation.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+using MediatR;
+
+namespace PropertyManager.ViewModels.Application.Landlords.Commands
+{
+    public class CreateLandlordNotifcation : INotification
+    {
+        [Required]
+        public string Id { get; set; }
+
+        [StringLength(maximumLength: 10)]
+        public string Title { get; set; }
+
+        [Required]
+        [Display(Name = "First Name")]
+        [StringLength(maximumLength: 50)]
+        public string FirstName { get; set; }
+
+        [Display(Name = "Middle Name(s)")]
+        [StringLength(maximumLength: 256)]
+        public string MiddleNames { get; set; }
+
+        [Required]
+        [Display(Name = "Last Name")]
+        [StringLength(maximumLength: 50)]
+        public string LastName { get; set; }
+
+        [Required]
+        [EmailAddress]
+        public string Email { get; set; }
+
+        [Required]
+        [Display(Name = "Mobile Phone")]
+        [StringLength(maximumLength: 20)]
+        public string MobilePhone { get; set; }
+
+        [Display(Name = "Home Phone")]
+        [StringLength(maximumLength: 20)]
+        public string HomePhone { get; set; }
+
+        [Required]
+        [Display(Name = "Register Number")]
+        [StringLength(maximumLength: 50)]
+        public string RegisterNumber { get; set; }
+
+        [Required]
+        public DateTime Dob  { get; set; }
+    }
+}
--- PropertyManager/PropertyManager.Web.UI/Controllers/AuthController.cs
+++ PropertyManager/PropertyManager.Web.UI/Controllers/AuthController.cs
@@ -1,6 +1,11 @@
-﻿using System.Net.Http;
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Security.Claims;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.Extensions.Configuration;
+using PropertyManager.Web.UI.Common;
 
 namespace PropertyManager.Web.UI.Controllers
 {
@@ -12,6 +17,32 @@ namespace PropertyManager.Web.UI.Controllers
             IConfiguration configuration)
             : base(httpClientFactory, configuration)
         {
+        }
+
+        protected AuthenticationHeaderValue GetAuthHeader()
+        {
+            var accessTokenClaim = GetClaimsFromUser()
+                .Where(x => x.Type.Equals(ProjectConstants.ACCESS_TOKEN_CLAIM))
+                .FirstOrDefault();
+
+            return new AuthenticationHeaderValue(
+                ProjectConstants.BEARER,
+                accessTokenClaim.Value);
+        }
+
+        protected string GetUserId()
+        {
+            var idClaim = GetClaimsFromUser()
+                .Where(x => x.Type.Equals(ClaimTypes.NameIdentifier))
+                .FirstOrDefault();
+
+            return idClaim.Value;
+        }
+
+        private IEnumerable<Claim> GetClaimsFromUser()
+        {
+            var identity = User.Identity as ClaimsIdentity;
+            return identity.Claims;
         }
     }
 }
