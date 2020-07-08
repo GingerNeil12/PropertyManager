a2fe49176a633442e388015e3eececd259048cd8
Neil Earlam <neil.earlam2019@outlook.com>
2020-07-08 22:07:34 +0100

    Working with the GetLandlordsRequestHandler

---
+++ PropertyManager/PropertyManager.Application/Exceptions/NotFoundException.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace PropertyManager.Application.Exceptions
+{
+    public class NotFoundException : Exception
+    {
+        public NotFoundException(string message)
+            : base(message)
+        {
+
+        }
+
+        public NotFoundException()
+            : this("Resource not found.")
+        {
+
+        }
+    }
+}
