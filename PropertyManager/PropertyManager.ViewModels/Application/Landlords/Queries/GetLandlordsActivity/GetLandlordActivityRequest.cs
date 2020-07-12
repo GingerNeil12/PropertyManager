10cafe767f7e431534d653ecbf81c88929e9c129
Neil Earlam <neil.earlam2019@outlook.com>
2020-07-12 02:19:26 +0100

    Created the Activity View Models

---
+++ PropertyManager/PropertyManager.ViewModels/Application/Landlords/Queries/GetLandlordsActivity/GetLandlordActivityRequest.cs
@@ -0,0 +1,11 @@
+﻿using MediatR;
+using PropertyManager.ViewModels.Application.Common;
+
+namespace PropertyManager.ViewModels.Application.Landlords.Queries.GetLandlordsActivity
+{
+    public class GetLandlordActivityRequest : IRequest<LandlordActivityViewModel>
+    {
+        public string LandlordId { get; set; }
+        public FilterDto Filter { get; set; }
+    }
+}
---
+++ PropertyManager/PropertyManager.ViewModels/Application/Landlords/Queries/GetLandlordsActivity/LandlordActivityDto.cs
@@ -0,0 +1,11 @@
+﻿namespace PropertyManager.ViewModels.Application.Landlords.Queries.GetLandlordsActivity
+{
+    public class LandlordActivityDto
+    {
+        public int Id { get; set; }
+        public string Action { get; set; }
+        public string Description { get; set; }
+        public string UserName { get; set; }
+        public string HappenedOn { get; set; }
+    }
+}
